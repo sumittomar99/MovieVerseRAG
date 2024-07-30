6be06641b8c6e86043e99e9cb1e7d4a36c7a4de7
Sumit Tomar <31665221+sumittomar99@users.noreply.github.com>
2024-07-30 22:38:10 +0530

    Update generate_story.py

--- scripts/generate_story.py
+++ scripts/generate_story.py
@@ -6,7 +6,12 @@ def generate_mixed_story(user_story, movie_descriptions, openai_api_key):
     prompts = generate_prompts(user_story, movie_descriptions)
 
     combined_prompt = " ".join(prompts)
-    system_prompt = "Your a great Movie script writer and Story writer. Your task is to generate well formulated and structured story with each details to support story. Make it readable to the users with different sections of the story. Use Markdown to make it appealing."
+    system_prompt = """Your a great Movie script writer and Story writer. Your task is to generate well formulated and structured story with each details to support story. Make it readable to the users with different sections of the story. Use Markdown to make it appealing.
+    Rules to follow:
+    1. Title must be given
+    2. Not necessarily the story should have converstaion
+    3. Order the sequence with 7 step of story writing format rule.
+    """
     generated_story, error_message  = generate_response(system_prompt + combined_prompt, openai_api_key)
     
     return generated_story, error_message 
