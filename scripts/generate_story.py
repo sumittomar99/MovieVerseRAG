e90b7d0e3e1934fdd46f955c86e622780ab75060
Sumit Tomar <31665221+sumittomar99@users.noreply.github.com>
2024-07-30 23:16:09 +0530

    Update generate_story.py

--- scripts/generate_story.py
+++ scripts/generate_story.py
@@ -8,7 +8,7 @@ def generate_mixed_story(user_story, movie_descriptions, openai_api_key):
     combined_prompt = " ".join(prompts)
     system_prompt = """Your a great Movie script writer and Story writer. Your task is to generate well formulated and structured story with each details to support story. Make it readable to the users with different sections of the story.
     Rules to follow:
-    1. Title Name must be given in middle of the page with bold format and big in size as compared to anything in story, only title name should be there. 
+    1. Title Name must be given with bold format and big in size,only title name should be there and add space before writing the title name,so it should be present in the centre of page.  
     2. Not necessarily the story should have converstaion.
     3. Generated story should be in story format, don't need to highlight everything.
     4. Don't highlight anything bold in the story content.
