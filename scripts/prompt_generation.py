36cde234844474b68d7a9bf654994070758ee733
shubham-shinde <shindes0321@gmail.com>
2024-07-30 17:22:49 +0530

    updated

--- scripts/prompt_generation.py
+++ scripts/prompt_generation.py
@@ -5,7 +5,7 @@ def generate_prompts(user_story, movie_descriptions):
     prompts = []
     
     # Load spaCy English model
-    nlp = spacy.load("en_core_web_sm")
+    nlp = spacy.load("en_core_web_sm-3.7.1")
 
     # Function to replace character names
     def replace_character_names(description):
